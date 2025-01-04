c4e8a709c4c1cf8da14ca5cf70cd64051ee3ebcc
shawn buckhannon <shawnbuckhannon@yahoo.com>
2025-01-04 18:40:58 -0500

    Add auth routes

--- simple-login/server.js
+++ simple-login/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -11,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 // MongoDB Connection
