8551caf69fa5801712db2c6feb54334b66866853
Shawn Buckhannon <shawnbuckhannon@yahoo.com>
2025-01-11 07:32:14 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -379,17 +379,6 @@ app.get('/available-teams', async (req, res) => {
     res.status(500).send('Error fetching teams.');
   }
 });
-const mongoose = require('mongoose');
-
-const teamSchema = new mongoose.Schema({
-  id: String,
-  abbreviation: String,
-  name: String,
-  conference: String,
-  division: String,
-});
-
-const Team = mongoose.model('Team', teamSchema);
 app.get('/available-teams', async (req, res) => {
   try {
     const teams = await Team.find(); // Adjust the query if needed
