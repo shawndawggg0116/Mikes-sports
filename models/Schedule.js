49a3cc23dddc06df1b621e1579a7215e08a47421
Shawn Buckhannon <shawnbuckhannon@yahoo.com>
2025-01-15 11:45:04 -0500

    Add files via upload

--- models/Schedule.js
+++ models/Schedule.js
@@ -1,19 +1,20 @@
 const mongoose = require('mongoose');
 
 const scheduleSchema = new mongoose.Schema({
-  week: { type: Number, required: true },
-  games: [
-    {
-      homeTeam: { type: String, required: true },
-      awayTeam: { type: String, required: true },
-      date: { type: String, required: true },
-      time: { type: String, required: true },
-      location: { type: String, required: true },
-      homeTeamScore: { type: Number, default: null },
-      awayTeamScore: { type: Number, default: null },
-      status: { type: String, default: "Scheduled" },
-    },
-  ],
+    week: { type: Number, required: true },
+    games: { 
+        type: [{
+            homeTeam: { type: String, required: true },
+            awayTeam: { type: String, required: true },
+            date: { type: String, required: true },
+            time: { type: String, required: true },
+            location: { type: String, required: true },
+            homeTeamScore: { type: Number, default: null },
+            awayTeamScore: { type: Number, default: null },
+            status: { type: String, default: "Scheduled" }
+        }],
+        required: true
+    }
 });
 
-module.exports = mongoose.model('Schedule', scheduleSchema);
+module.exports = mongoose.model('Schedule', scheduleSchema, 'schedules');
