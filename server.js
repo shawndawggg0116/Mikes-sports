58b20e8d020ac60f010f46dfd5543b9395c3a717
Shawn Buckhannon <shawnbuckhannon@yahoo.com>
2025-01-15 09:39:28 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// MongoDB connection
-mongoose.connect(
-  "mongodb+srv://shawnbuckhannon:S8h7a6wN@mikes-sports0new.pn8ro.mongodb.net/nfl-picks-app?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-).then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
 
 // Middleware
 app.use(express.json());
