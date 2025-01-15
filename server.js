762436c624e5baf0c6516f2d8c4558c73c6957fe
Shawn Buckhannon <shawnbuckhannon@yahoo.com>
2025-01-15 10:30:42 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -7,12 +7,7 @@ const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+
 
   mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
