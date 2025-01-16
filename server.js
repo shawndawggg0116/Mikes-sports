e63e5283995808867fb1815488dbef190b28d774
Shawn Buckhannon <shawnbuckhannon@yahoo.com>
2025-01-15 21:37:02 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const bcrypt = require('bcrypt');
 const path = require('path');
 const session = require('express-session');
 const cron = require('node-cron');
-const Schedule = require('./path/to/your/Schedule'); // Adjust the path if necessary
+const Schedule = require('./Schedule');
+
 
 
 const app = express();
