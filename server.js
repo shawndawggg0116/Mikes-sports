6b66c72f423efac9c0ed6467eb3d7d331dbe67b2
Shawn Buckhannon <shawnbuckhannon@yahoo.com>
2025-01-26 14:50:27 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const socketIo = require('socket.io');
 const http = require('http');
 const path = require('path');
+const bcrypt = require('bcrypt');
 
 // Initialize Express app and server
 const app = express();
@@ -16,9 +17,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB connection
-const mongoUri = "mongodb+srv://shawnbuckhannon:S8h7a6wN@mikes-sports0new.pn8ro.mongodb.net/?retryWrites=true&w=majority&appName=mikes-sports0new";
+const mongoUri = "mongodb+srv://shawnbuckhannon:S8h7a6wN@mikes-sports0new.pn8ro.mongodb.net/nfl-picks-app?retryWrites=true&w=majority&appName=mikes-sports0new";
 mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
+  .then(() => console.log('MongoDB connected to nfl-picks-app database'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 // MongoDB Schemas and Models
@@ -37,18 +38,31 @@ const ScheduleSchema = new mongoose.Schema({
   status: String
 });
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema, 'users');
 const Schedule = mongoose.model('Schedule', ScheduleSchema);
 
 // Routes
 // Login endpoint
 app.post('/api/login', async (req, res) => {
   const { username, password } = req.body;
-  const user = await User.findOne({ username, password });
-  if (user) {
+  try {
+    // Find the user by username
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).send({ success: false, message: 'Invalid credentials' });
+    }
+
+    // Compare the hashed password with the provided password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).send({ success: false, message: 'Invalid credentials' });
+    }
+
+    // Successful login
     res.status(200).send({ success: true });
-  } else {
-    res.status(401).send({ success: false, message: 'Invalid credentials' });
+  } catch (err) {
+    console.error('Login error:', err);
+    res.status(500).send({ success: false, message: 'Server error' });
   }
 });
 
