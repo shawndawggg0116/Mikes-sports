38986224bad3da558315dc58dd5c72a8fb6a6e17
Shawn Buckhannon <shawnbuckhannon@yahoo.com>
2025-02-14 15:43:37 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -31,22 +31,6 @@ const authenticateToken = (req, res, next) => {
 };
 
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js').then(registration => {
-    console.log('Service Worker registered with scope:', registration.scope);
-
-    // Request permission for notifications
-    Notification.requestPermission().then(permission => {
-      if (permission === 'granted') {
-        console.log('Notification permission granted.');
-      } else {
-        console.log('Notification permission denied.');
-      }
-    });
-  }).catch(error => {
-    console.error('Service Worker registration failed:', error);
-  });
-}
 
 
 // WebSocket logic
