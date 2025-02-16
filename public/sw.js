34938b704e4125dabe015647d614ad6f1e66afde
Shawn Buckhannon <shawnbuckhannon@yahoo.com>
2025-02-16 18:57:58 -0500

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,12 @@
 self.addEventListener('push', event => {
-    const data = event.data.json();
-    self.registration.showNotification(data.notification.title, {
-        body: data.notification.body,
-        icon: data.notification.icon
-    });
+    if (event.data) {
+        const data = event.data.json();
+        self.registration.showNotification(data.notification.title, {
+            body: data.notification.body,
+            icon: data.notification.icon,
+            vibrate: [200, 100, 200], // Optional: Vibrates on receive
+        });
+    } else {
+        console.log('Push event but no data');
+    }
 });
