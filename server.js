11b1165c168d2a9a54598aef578f9bee3ef466b2
Shawn Buckhannon <shawnbuckhannon@yahoo.com>
2025-02-15 20:04:10 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const path = require('path');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
-const moment = require('moment-timezone'); // Include moment-timezone
-const http = require('http'); // ✅ Require 'http' BEFORE using it
-const { Server } = require('socket.io'); // ✅ Import Socket.io
+const moment = require('moment-timezone');
+const http = require('http'); 
+const { Server } = require('socket.io');
 const admin = require("firebase-admin");
+
 
 
 // server.js (relevant part)
